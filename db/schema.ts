446edd8c3784601c180fe3a60ee5e174a396930f
AlanNin <alanbusinessnin@gmail.com>
2024-12-11 19:11:36 -0400

    db schema update

--- db/schema.ts
+++ db/schema.ts
@@ -1,10 +1,27 @@
 import { sql } from "drizzle-orm";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+export const notebooks = sqliteTable("notebooks", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  color: text().notNull(),
+  created_at: text("created_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  updated_at: text("updated_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`)
+    .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
+});
+
 export const notes = sqliteTable("notes", {
   id: int().primaryKey({ autoIncrement: true }),
   title: text().notNull(),
   content: text().notNull(),
+  notebook_id: int().references(() => notebooks.id, {
+    onDelete: "cascade",
+    onUpdate: "cascade",
+  }),
   created_at: text("created_at")
     .notNull()
     .default(sql`CURRENT_TIMESTAMP`),
